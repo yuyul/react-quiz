a5dce42d1503def86e1301a69678dc970da5199f
Juan Julián Caro Sancho <carsanju@hotmail.com>
2022-03-22 21:44:09 +0100

    Add next and previous button

--- src/pages/Play/index.jsx
+++ src/pages/Play/index.jsx
@@ -3,16 +3,31 @@ import data from './questions.json'
 import './play.css'
 
 export default function Play() {
+    const [questions, setQuestions] = useState([])
+    const [questionNumber, setQuestionNumber] = useState(0)
+    const [answers, setAnswers] = useState([])
     const [question, setQuestion] = useState({})
 
     useEffect(() => {
-        const questionNumber = Math.floor(Math.random() * data.questions.length);
-        setQuestion(data.questions[questionNumber])
+        const shuffledQuestions = data.questions.sort((a, b) => 0.5 - Math.random());
+        setQuestions(shuffledQuestions)
+        setQuestion(shuffledQuestions[0])
+        console.log("Load")
     }, [])
+
+    useEffect(() => {
+        console.log("update question")
+        if (questions.length > 0) {
+            setQuestion(questions[questionNumber])
+        }
+    }, [questionNumber])
+
+
 
     return(
         <div>
             <h1>Play</h1>
+            <h2>Question {questionNumber}</h2>{}
             <img src={question.image} />
             <h2>{question.description}</h2>
             <ol className='answers'>
@@ -22,6 +37,8 @@ export default function Play() {
                     </li>
                 ))}
             </ol>
+            {questionNumber > 0 && (<button onClick={() => setQuestionNumber(questionNumber-1)}>Previous</button>)}
+            {questionNumber < questions.length - 1 && (<button onClick={() => setQuestionNumber(questionNumber+1)}>Next</button>)}
         </div>
     )
 }
